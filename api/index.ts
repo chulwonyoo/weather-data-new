bafe89707d9a31623646fedcf081601bc8891c5a
Lechl <lechlfafa001@gmail.com>
2025-09-05 13:09:13 +0900

    first commit

---
+++ api/index.ts
@@ -0,0 +1,34 @@
+var express = require("express");
+var app = express();
+
+app.get("/weather", function (req: any, res: any) {
+    const { serviceKey, numOfRows, pageNo, base_date, base_time, nx, ny } =
+        req.query;
+
+    var api_url =
+        "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst?";
+    var request = require("request");
+    var options = {
+        url: api_url,
+        qs: { serviceKey, numOfRows, pageNo, base_date, base_time, nx, ny },
+    };
+
+    request.get(options, function (error: any, response: any, body: any) {
+        if (!error && response.statusCode == 200) {
+            res.writeHead(200, { "Content-Type": "application/xml;charset=utf-8" });
+            res.end(body);
+        } else {
+            res.status(response.statusCode).end();
+            console.log("error = " + response.statusCode);
+        }
+    });
+});
+
+app.listen(3000, function () {
+    console.log(
+        "http://127.0.0.1:3000/weather?serviceKey=D6YAORFazkE%2FNeOMZMJzAjmrbekg7UM66RzfOyaYP2IhpI0tNiySRgpDuxDseNjZOgDIqsH7%2FC5YPk8Vuyvk4Q%3D%3D&numOfRows=10&pageNo=1&base_date=20250905&base_time=0600&nx=60&ny=127 app listening on port 3000!"
+    );
+});
+
+console.log("서버가 시작되었습니다!");
+console.log("현재 시간:", new Date());
